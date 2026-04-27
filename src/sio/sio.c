9bbc32728f80f23814f4fa9b7a8523f94cd8448e
arjun-myanger <admyanger@outlook.com>
2026-04-27 12:03:25 +0100

    sio: byte-level register read/write dispatch

--- src/sio/sio.c
+++ src/sio/sio.c
@@ -11,12 +11,47 @@ void sio_init(SIO* sio, InterruptController* interrupts, LinkPeer* peer) {
 }
 
 uint8_t sio_read8(SIO* sio, uint32_t offset) {
-    (void)sio; (void)offset;
-    return 0;
+    switch (offset) {
+    case 0x120: return (uint8_t)sio->siomulti[0];
+    case 0x121: return (uint8_t)(sio->siomulti[0] >> 8);
+    case 0x122: return (uint8_t)sio->siomulti[1];
+    case 0x123: return (uint8_t)(sio->siomulti[1] >> 8);
+    case 0x124: return (uint8_t)sio->siomulti[2];
+    case 0x125: return (uint8_t)(sio->siomulti[2] >> 8);
+    case 0x126: return (uint8_t)sio->siomulti[3];
+    case 0x127: return (uint8_t)(sio->siomulti[3] >> 8);
+    case 0x128: return (uint8_t)sio->siocnt;
+    case 0x129: return (uint8_t)(sio->siocnt >> 8);
+    case 0x12A: return (uint8_t)sio->siomlt_send;
+    case 0x12B: return (uint8_t)(sio->siomlt_send >> 8);
+    case 0x134: return (uint8_t)sio->rcnt;
+    case 0x135: return (uint8_t)(sio->rcnt >> 8);
+    default:    return 0;
+    }
 }
 
 void sio_write8(SIO* sio, uint32_t offset, uint8_t val) {
-    (void)sio; (void)offset; (void)val;
+    switch (offset) {
+    case 0x120: sio->siomulti[0] = (sio->siomulti[0] & 0xFF00) | val; break;
+    case 0x121: sio->siomulti[0] = (sio->siomulti[0] & 0x00FF) | ((uint16_t)val << 8); break;
+    case 0x122: sio->siomulti[1] = (sio->siomulti[1] & 0xFF00) | val; break;
+    case 0x123: sio->siomulti[1] = (sio->siomulti[1] & 0x00FF) | ((uint16_t)val << 8); break;
+    case 0x124: sio->siomulti[2] = (sio->siomulti[2] & 0xFF00) | val; break;
+    case 0x125: sio->siomulti[2] = (sio->siomulti[2] & 0x00FF) | ((uint16_t)val << 8); break;
+    case 0x126: sio->siomulti[3] = (sio->siomulti[3] & 0xFF00) | val; break;
+    case 0x127: sio->siomulti[3] = (sio->siomulti[3] & 0x00FF) | ((uint16_t)val << 8); break;
+    case 0x128:
+        sio->siocnt = (sio->siocnt & 0xFF00) | val;
+        break;
+    case 0x129:
+        sio->siocnt = (sio->siocnt & 0x00FF) | ((uint16_t)val << 8);
+        break;
+    case 0x12A: sio->siomlt_send = (sio->siomlt_send & 0xFF00) | val; break;
+    case 0x12B: sio->siomlt_send = (sio->siomlt_send & 0x00FF) | ((uint16_t)val << 8); break;
+    case 0x134: sio->rcnt = (sio->rcnt & 0xFF00) | val; break;
+    case 0x135: sio->rcnt = (sio->rcnt & 0x00FF) | ((uint16_t)val << 8); break;
+    default: break;
+    }
 }
 
 void sio_tick(SIO* sio, int cycles) {
--- tests/test_sio.c
+++ tests/test_sio.c
@@ -24,7 +24,75 @@ TEST(sio_init_zeros_state) {
     ASSERT_TRUE(sio.peer == NULL);
 }
 
+TEST(sio_siocnt_writes_low_byte) {
+    SIO sio;
+    InterruptController ic;
+    interrupt_init(&ic);
+    sio_init(&sio, &ic, NULL);
+
+    sio_write8(&sio, 0x128, 0xAB);
+    ASSERT_EQ_HEX(sio.siocnt & 0xFF, 0xAB);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x128), 0xAB);
+}
+
+TEST(sio_siocnt_writes_high_byte) {
+    SIO sio;
+    InterruptController ic;
+    interrupt_init(&ic);
+    sio_init(&sio, &ic, NULL);
+
+    sio_write8(&sio, 0x129, 0x40); // bit 14 = IRQ enable
+    ASSERT_EQ_HEX(sio.siocnt & 0xFF00, 0x4000);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x129), 0x40);
+}
+
+TEST(sio_rcnt_writes_round_trip) {
+    SIO sio;
+    InterruptController ic;
+    interrupt_init(&ic);
+    sio_init(&sio, &ic, NULL);
+
+    sio_write8(&sio, 0x134, 0x12);
+    sio_write8(&sio, 0x135, 0x80);
+    ASSERT_EQ_HEX(sio.rcnt, 0x8012);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x134), 0x12);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x135), 0x80);
+}
+
+TEST(sio_siomlt_send_round_trip) {
+    SIO sio;
+    InterruptController ic;
+    interrupt_init(&ic);
+    sio_init(&sio, &ic, NULL);
+
+    sio_write8(&sio, 0x12A, 0xCD);
+    sio_write8(&sio, 0x12B, 0xAB);
+    ASSERT_EQ_HEX(sio.siomlt_send, 0xABCD);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x12A), 0xCD);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x12B), 0xAB);
+}
+
+TEST(sio_siomulti_reads_back) {
+    SIO sio;
+    InterruptController ic;
+    interrupt_init(&ic);
+    sio_init(&sio, &ic, NULL);
+
+    sio.siomulti[0] = 0x1234;
+    sio.siomulti[1] = 0xABCD;
+
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x120), 0x34);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x121), 0x12);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x122), 0xCD);
+    ASSERT_EQ_HEX(sio_read8(&sio, 0x123), 0xAB);
+}
+
 void run_sio_tests(void) {
     printf("\nSIO tests:\n");
     RUN_TEST(sio_init_zeros_state);
+    RUN_TEST(sio_siocnt_writes_low_byte);
+    RUN_TEST(sio_siocnt_writes_high_byte);
+    RUN_TEST(sio_rcnt_writes_round_trip);
+    RUN_TEST(sio_siomlt_send_round_trip);
+    RUN_TEST(sio_siomulti_reads_back);
 }
